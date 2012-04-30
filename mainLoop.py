c268773cc9b382a06175b1ae8a68bc8d1d477c96
Josh Zimmerman <zimagic@gmail.com>
2012-04-30 00:19:15 -0400

    Fixed time-based bans, fixed deciding when to ban.

--- mainLoop.py
+++ mainLoop.py
@@ -23,66 +23,71 @@ setMode = False # have we set +B yet?
 unbans = {} # keep track of who we need to unban, and when.
 while True: #main REPL
     input = con.receive()
-    print input
-
-    mNick = messageParser.getNick(input)
-    mChan = messageParser.getChannel(input)
-    message = messageParser.getMessage(input)
+    # Unban if needed
+    cpy = unbans.copy()
+    for u in cpy:
+        if cpy[u][1] <= time.time():
+            con.sendMessage("MODE " + cpy[u][0] + " -b " + u)
+            del unbans[u]
     
-    if "End of /MOTD command." in input and not(setMode):
-        setMode = True
-        # print "Setting mode +B"
-        con.sendMessage("MODE " + nick + " +B") # indicate that we're a bot
-        con.sendMessage("PRIVMSG NickServ IDENTIFY short-ead-aup-y") 
-    
-    pingResponse = messageParser.pingHandler(input)
-    if pingResponse != "":
-        con.sendMessage("PONG " + pingResponse)
-        #print(repr(pingResponse))
-		
-    #print(repr(getMessage(input)))
-    
-    for u in unbans:
-        if unbans[u][1] <= time.time():
-            con.sendMessage("MODE " + unbans[u][0] + " -b " + u)
+    if input != "":
+        print input
 
-    if ('e' in message or 'E' in message) and ('foonetic.net' not in input) and setMode and mNick != nick:
-        print "Kicking " + mNick + " for " + message 
-        con.sendMessage("KICK " + mChan + " " + mNick)
-        con.sendMessage("MODE " + mChan + " +b " + mNick)
-        unbans[mNick] = (mChan, time.time() + 60)
+        mNick = messageParser.getNick(input)
+        mChan = messageParser.getChannel(input)
+        message = messageParser.getMessage(input)
+        
+        if "End of /MOTD command." in input and not(setMode):
+            setMode = True
+            # print "Setting mode +B"
+            con.sendMessage("MODE " + nick + " +B") # indicate that we're a bot
+            con.sendMessage("PRIVMSG NickServ IDENTIFY short-ead-aup-y") 
+        
+        pingResponse = messageParser.pingHandler(input)
+        if pingResponse != "":
+            con.sendMessage("PONG " + pingResponse)
+            #print(repr(pingResponse))
+            
+        #print(repr(getMessage(input)))
+        
+
+        if ('e' in message or 'E' in message) and messageParser.getMessageType(input) == "PRIVMSG" and mNick != nick:
+            print "Kicking " + mNick + " for " + message 
+            con.sendMessage("KICK " + mChan + " " + mNick)
+            con.sendMessage("MODE " + mChan + " +b " + mNick)
+            unbans[mNick] = (mChan, time.time() + 10)
 
 
-    if mNick in admins:  #admin-only commands
-        if messageParser.hasPrefix(input):  #to avoid accidental commands, ensure command is prefaced with the prefix
-            if messageParser.isQuit(input):
-                quitMessage = messageParser.partOrQuitMessage(input)
-				
-                if quitMessage != "":
-                    con.sendMessage("QUIT" + " :" + quitMessage)
-                else:
-                    con.sendMessage("QUIT")
-                break
-            if messageParser.isPart(input):
-                partMessage = messageParser.partOrQuitMessage(input)
-                #print(repr(partMessage))
+        if mNick in admins:  #admin-only commands
+            if messageParser.hasPrefix(input):  #to avoid accidental commands, ensure command is prefaced with the prefix
+                if messageParser.isQuit(input):
+                    quitMessage = messageParser.partOrQuitMessage(input)
+                    
+                    if quitMessage != "":
+                        con.sendMessage("QUIT" + " :" + quitMessage)
+                    else:
+                        con.sendMessage("QUIT")
+                    break
+                if messageParser.isPart(input):
+                    partMessage = messageParser.partOrQuitMessage(input)
+                    #print(repr(partMessage))
 
-                if partMessage != "":
-                    partMessage = " " + partMessage
-                    	
-                if messageParser.partDefault(input):
-                    toPart = mChan # part current chan if none specified
-                else:
-                    toPart = messageParser.getPartChannel(input)
-                #print(repr(toPart))
+                    if partMessage != "":
+                        partMessage = " " + partMessage
+                            
+                    if messageParser.partDefault(input):
+                        toPart = mChan # part current chan if none specified
+                    else:
+                        toPart = messageParser.getPartChannel(input)
+                    #print(repr(toPart))
+                    
+        
+                    if toPart != "":
+                        con.sendMessage("PART " + toPart + partMessage)
+                        #print(repr(toPart + partMessage))
                 
-	
-                if toPart != "":
-                    con.sendMessage("PART " + toPart + partMessage)
-                    #print(repr(toPart + partMessage))
-			
-        if messageParser.getMessageType(input) == "INVITE": #auto-join on any invite (by an admin)
-            con.sendMessage("JOIN " + message)
+            if messageParser.getMessageType(input) == "INVITE": #auto-join on any invite (by an admin)
+                con.sendMessage("JOIN " + message)
 
 time.sleep(0.5)		
 con.close()
