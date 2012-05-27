5ce091fefb1cc2abca9713c5e678a46795c10894
Josh Zimmerman <zimagic@gmail.com>
2012-05-26 20:42:50 -0700

    minor style changes

--- ioHandler.py
+++ ioHandler.py
@@ -8,25 +8,40 @@ class IRCConnection:
         self.connection.setblocking(0)
 	
     def sendMessage(self, toSend):
-        '''Helper function that sends the given string as an IRC message, appending \r\n.'''
-        self.connection.send(str(toSend + "\r\n")) #, "utf-8"))
+        '''Helper function that sends the given
+           string as an IRC message, appending \r\n.
+        
+        '''
+        self.connection.send(str(toSend + "\r\n")) 
 
     def receive(self):
-        '''Recieve 512 bytes from the connection (512 bytes == 1 message)'''
+        '''Recieve 512 bytes from the connection (512 bytes == 1 message)
+        
+        '''
+        # make sure we time out after 0.02 seconds so we unban quickly
         ready = select.select([self.connection], [], [], 0.02)
         if ready[0]:
-            return str(self.connection.recv(512)) #, "utf-8")
+            return str(self.connection.recv(512)) 
         else:
             return ""
 		
     def setNick(self, nick):
-        '''Sets the nick on this connection.'''
+        '''Sets the nick to given string.
+        
+        '''
         self.sendMessage("NICK " + nick)
 		
     def setUser(self, userName, hostName, serverName, realName):
-        '''Set the user'''
-        self.sendMessage("USER " + userName + " " + hostName + " " + serverName + " :" + realName)
+        '''Set the user info as given.
+        
+        '''
+        self.sendMessage("USER " + userName + " " +
+                                   hostName + " " +
+                                   serverName + " :" +
+                                   realName)
 
     def close(self):
-        '''Close the connection.''' 
+        '''Close the connection.
+        
+        ''' 
         self.connection.close()
--- messageParser.py
+++ messageParser.py
@@ -1,66 +1,81 @@
 import re
 
-prefix = "&"
+import config
 
-# :Emi!~chatzilla@hide-F95A65D1.hsd1.md.comcast.net PRIVMSG Emijoshzbot :&time UTC+4
+prefix = config.prefix
 
-#Returns the nick string from an IRC message, validating against the valid nick characters and length.
-# nick: letters, numbers, and the characters -_[]{}\|`^. The nickname cannot begin with a digit or a hyphen.
+# Useful test strings:
+# :Emi!~chatzilla@hide-F95A65D1.hsd1.md.comcast.net PRIVMSG Emijoshzbot :&quit test
+# IPv6: :TGB!~user@1A1ED16D:89725881:A8F36EFF:IP PRIVMSG #xkcd-abcdfghijklmnopqrstuvwxyz :ok.
+
+# Returns the nick string from an IRC message,
+#     validating against the valid nick characters and length.
+# nick: letters, numbers, and the characters -_[]{}\|`^.
+#     The nickname cannot begin with a digit or a hyphen.
 nick = re.compile(r":([^\d\-][A-Za-z0-9\-_[\]{}\\|`^]{0,29})")
 
-#Get the user-sent message from an IRC message
+# Get the user-sent message from an IRC PRIVMSG
 message = re.compile(r"[^:]+\ :(.*$)")
 
-#Get the type of message from an IRC message
+# Get the type of message from an IRC message
+#   (e.g. PRIVMSG, KICK, etc)
 messageType = re.compile(" ([A-Z]*)")
 
-#Get the channel a message was sent to from an IRC message
+# Get the channel a message was sent to from an IRC message
 channel = re.compile("\S+ \S+ (\S+)")
 
-#Determines whether the sent message is a PING; if it was, enables returning of the needed message to PONG
+# Determines whether the sent message is a PING;
 ping = re.compile("^PING :(.*?)\r")
 
-#Determines whether or not something is a user-given part command
+# Determines whether or not something is a user-given part command
 part = re.compile("^" + prefix + "part")
 
-#Determines whether a part command has channel arguments
+# Determines whether a part command has channel arguments
 partNoArgs = re.compile(prefix + "part(\r| [^&#+!].*)")
 
-#If given a part or quit command, get the message the user specified for the bot to part or quit with (e.g., &part #azure Message text.)
+# If given a part or quit command, get the message the user specified for the
+#   bot to part or quit with (e.g., &part #foo Message text.)
 partQuitMessage = re.compile(prefix + "(?:quit|part)(?: [#&+!]\w+)* (.*?)\r")
 
-#Given a user message, determine whether it's a command to the bot to quit
+# Given a user message, determine whether it's a command to the bot to quit
 quitPattern = re.compile("^" + prefix + "quit")
 
-#Get the channel from which to part, given a part command from a user
+# Get the channel from which to part, given a part command from a user
 channelToPart = re.compile("part ([&#+!][^, :]+)")
 
-#Get the UTC offset (e.g., UTC+5:00) from a time command.
-utcOffset = re.compile("UTC([+-][0-9]{1,2}):([0-9]{2})")
 
-def hasPrefix(ircMessage):
-    '''Returns true if and only if the user-sent part of the message has the command prefix.'''
-    message = getMessage(ircMessage)
-    return message.startswith(prefix)
-
-def getNick(ircMessage):
-    '''Given an IRC message, returns the nick string (only if it's all valid chars).'''
-    nickMatch = re.search(nick, ircMessage)
-    try:
-        return nickMatch.group(1)
-    except:
-        return ""
-        
 def getMessage(ircMessage):
-    '''Given an IRC message, returns the message being sent by the user.'''
+    '''Given an IRC message, returns the message being sent by the user.
+    
+    '''
     messageMatch = re.search(message, ircMessage)
     try:
         return messageMatch.group(1)
     except:
         return ""
 
+def hasPrefix(ircMessage):
+    '''Returns true iff the user-sent message has our command prefix.
+    
+    '''
+    message = getMessage(ircMessage)
+    return message.startswith(prefix)
+
+def getNick(ircMessage):
+    '''Given an IRC message, returns the nick string (only if it's valid).
+    
+    '''
+    nickMatch = re.search(nick, ircMessage)
+    try:
+        return nickMatch.group(1)
+    except:
+        return ""
+        
 def getMessageType(ircMessage):
-    '''Given a properly-formatted IRC message, return type of message (i.e. PRIVMSG, INVITE, etc)'''
+    '''Given a properly-formatted IRC message, return type of message
+        (e.g. PRIVMSG, INVITE, etc)
+        
+    '''
     messageTypeMatch = re.search(messageType, ircMessage)
     try:
         return messageTypeMatch.group(1)
@@ -68,7 +83,9 @@ def getMessageType(ircMessage):
         return ""
         
 def getChannel(ircMessage):
-    '''Given an IRC message, return the channel in which it was sent.'''
+    '''Given an IRC message, return the channel in which it was sent.
+    
+    '''
     channelMatch = re.search(channel, ircMessage)
     try:
         return channelMatch.group(1)
@@ -76,7 +93,10 @@ def getChannel(ircMessage):
         return ""
 
 def pingHandler(ircMessage):
-    '''Given an IRC message, return the server to PONG if it's a PING, else the empty string.'''
+    '''Given an IRC message,
+    return the server to PONG if it's a PING, else the empty string.
+    
+    '''
     pingMatch = re.search(ping, ircMessage)
     try:
         return pingMatch.group(1)
@@ -84,7 +104,9 @@ def pingHandler(ircMessage):
         return ""
 
 def isPart(ircMessage):
-    '''Returns true if and only if the given message is a part command'''
+    '''Returns true if and only if the given message is a part command
+    
+    '''
     message = getMessage(ircMessage)
     res = re.search(part, message)
     if res == None:
@@ -93,7 +115,10 @@ def isPart(ircMessage):
         return True
     
 def partDefault(ircMessage):
-    '''Returns true if and only if the message is a part command with no channel arguments'''
+    '''Returns true iff the message is a part command without
+        channel arguments
+        
+    '''
     message = getMessage(ircMessage)
     res = re.search(partNoArgs, message)
     if res == None:
@@ -102,7 +127,9 @@ def partDefault(ircMessage):
         return True
 
 def partOrQuitMessage(ircMessage):
-    '''Given a message which is a part or quit command, find the message to be sent with the quit (e.g. QUIT Message text -> Message text)'''
+    '''Given a message which is a part or quit command,
+        find the message to be sent with the quit
+        (e.g. QUIT Message text -> Message text)'''
     partQuitMessageMatch = re.search(partQuitMessage, ircMessage)
     try:
         return partQuitMessageMatch.group(1)
@@ -125,10 +152,3 @@ def getPartChannel(ircMessage):
     except:
         return ""
         
-def getUTCOffset(ircMessage):
-    '''Given an irc message (a time command, containing a UTC offset), return the offset as a number.'''
-    utcMatch = re.search(utcOffset, ircMessage)
-    try:
-        return [utcMatch.group(1), utcMatch.group(2)]
-    except:
-        return ["0", "0"]
