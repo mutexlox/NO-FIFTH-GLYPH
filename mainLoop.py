bfef1267df61b52fb39909d2340c79ca1921acd9
Josh Zimmerman <zimagic@gmail.com>
2012-05-26 20:43:19 -0700

    More extensible; now takes server and chan arguments from command-line

--- mainLoop.py
+++ mainLoop.py
@@ -2,103 +2,104 @@ import re
 import time
 import sys
 
+import config
 import ioHandler
 import messageParser
 
-admins = set(["joshz"])
-prefix = "&"
-nick = "noFifthGlyph"
 
-userName = "joshzNoFifthBot"
-hostName = "joshz"
-serverName = "irc.foonetic.net"
-realName = "joshz"
+def repl(server, password, chans):
 
-con = ioHandler.IRCConnection("irc.foonetic.net")
+    con = ioHandler.IRCConnection(server)
 
-con.setNick(nick)
-con.setUser(userName, hostName, serverName, realName)
+    con.setNick(config.nick)
+    con.setUser(config.userName, config.hostName, server, config.realName)
 
-setMode = False # have we set +B yet?
+    setMode = False # have we set +B yet?
 
-unbans = {} # keep track of who we need to unban, and when.
-while True: #main REPL
-    input = con.receive()
-    # Unban if needed
-    cpy = unbans.copy()
-    for u in cpy:
-        if cpy[u][1] <= time.time():
-            con.sendMessage("MODE " + cpy[u][0] + " -b " + u)
-            del unbans[u]
-    
-    if input != "":
-        print input
-
-        mNick = messageParser.getNick(input)
-        mChan = messageParser.getChannel(input)
-        message = messageParser.getMessage(input)
+    unbans = {} # keep track of who we need to unban, and when.
+    while True: #main REPL
+        input = con.receive()
+        # Unban if needed
+        cpy = unbans.copy()
+        for user in cpy: # cpy[u] is a tuple with (chan, time)
+            if cpy[user][1] <= time.time():
+                con.sendMessage("MODE " + cpy[user][0] + " -b " + user)
+                del unbans[user]
         
-        if "End of /MOTD command." in input and not(setMode):
-            setMode = True
-            # print "Setting mode +B"
-            con.sendMessage("MODE " + nick + " +B") # indicate that we're a bot
-            con.sendMessage("PRIVMSG NickServ IDENTIFY short-ead-aup-y") 
-            if len(sys.argv) > 1:
-                con.sendMessage("JOIN #" + sys.argv[1])
-        
-        pingResponse = messageParser.pingHandler(input)
-        if pingResponse != "":
-            con.sendMessage("PONG " + pingResponse)
-            #print(repr(pingResponse))
+        if input != "":
+            print input
+
+            mNick = messageParser.getNick(input)
+            mChan = messageParser.getChannel(input)
+            message = messageParser.getMessage(input)
             
-        #print(repr(getMessage(input)))
-        
-
-        if ('e' in message or 'E' in message) and \
-                messageParser.getMessageType(input) == "PRIVMSG" and \
-                mNick != nick: # don't kick ourselves
+            if "End of /MOTD command." in input and not(setMode):
+                setMode = True
+                # print "Setting mode +B"
+                con.sendMessage("MODE " + config.nick + " +B") # bot flag
+                con.sendMessage("PRIVMSG NickServ IDENTIFY " + password) 
+                for chan in chans:
+                    con.sendMessage("JOIN " + chan)
             
-            badWord = message
-            for word in message.split():
-                if 'e' in word or 'E' in word:
-                    badWord = word
-                    break
-            print "Kicking " + mNick + " for " + badWord
-            con.sendMessage("MODE " + mChan + " +b " + mNick)
-            con.sendMessage("KICK " + mChan + " " + mNick + " :" + badWord)
-            unbans[mNick] = (mChan, time.time() + 15)
-
-
-        if mNick in admins:  #admin-only commands
-            if messageParser.hasPrefix(input):  #to avoid accidental commands, ensure command is prefaced with the prefix
-                if messageParser.isQuit(input):
-                    quitMessage = messageParser.partOrQuitMessage(input)
-                    
-                    if quitMessage != "":
-                        con.sendMessage("QUIT :" + quitMessage)
-                    else:
-                        con.sendMessage("QUIT")
-                    break
-                if messageParser.isPart(input):
-                    partMessage = messageParser.partOrQuitMessage(input)
-                    #print(repr(partMessage))
-
-                    if partMessage != "":
-                        partMessage = " " + partMessage
-                            
-                    if messageParser.partDefault(input):
-                        toPart = mChan # part current chan if none specified
-                    else:
-                        toPart = messageParser.getPartChannel(input)
-                    #print(repr(toPart))
-                    
-        
-                    if toPart != "":
-                        con.sendMessage("PART " + toPart + partMessage)
-                        #print(repr(toPart + partMessage))
+            pingResponse = messageParser.pingHandler(input)
+            if pingResponse != "":
+                con.sendMessage("PONG " + pingResponse)
                 
-            if messageParser.getMessageType(input) == "INVITE": #auto-join on any invite (by an admin)
-                con.sendMessage("JOIN " + message)
 
-time.sleep(0.5)		
-con.close()
+            if ('e' in message or 'E' in message) and \
+                    messageParser.getMessageType(input) == "PRIVMSG" and \
+                    mNick != nick: # don't kick ourselves
+                
+                badWord = ""
+                for word in message.split():
+                    if 'e' in word or 'E' in word:
+                        badWord = word
+                        break
+                
+                con.sendMessage("MODE " + mChan + " +b " + mNick)
+                con.sendMessage("KICK " + mChan + " " + mNick + " :" + badWord)
+                unbans[mNick] = (mChan, time.time() + 15)
+
+
+            if mNick in config.admins:  #admin-only commands
+                #to avoid accidental commands, ensure command has our prefix
+                if messageParser.hasPrefix(input):  
+                    if messageParser.isQuit(input):
+                        quitMessage = messageParser.partOrQuitMessage(input)
+                        
+                        if quitMessage != "":
+                            con.sendMessage("QUIT :" + quitMessage)
+                        else:
+                            con.sendMessage("QUIT")
+                        break
+                    if messageParser.isPart(input):
+                        partMessage = messageParser.partOrQuitMessage(input)
+                        #print(repr(partMessage))
+
+                        if partMessage != "":
+                            partMessage = " " + partMessage
+                                
+                        if messageParser.partDefault(input):
+                            toPart = mChan # part current chan if none specified
+                        else:
+                            toPart = messageParser.getPartChannel(input)
+           
+
+                        if toPart != "":
+                            con.sendMessage("PART " + toPart + partMessage)
+                            #print(repr(toPart + partMessage))
+                #auto-join on any invite (by an admin) 
+                if messageParser.getMessageType(input) == "INVITE": 
+                    con.sendMessage("JOIN " + message)
+
+    time.sleep(0.5)		
+    con.close()
+
+def main():
+    if len(sys.argv) < 3:
+        print "Usage: python mainLoop.py server password [chans]"
+    else:
+        repl(sys.argv[1], sys.argv[2], sys.argv[3:])
+
+if __name__ == '__main__':
+    main()
