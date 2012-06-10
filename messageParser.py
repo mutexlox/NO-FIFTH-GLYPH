79b004205f9ff4574b1e3985903b40caf029b8f8
Josh Zimmerman <zimagic@gmail.com>
2012-06-09 19:24:42 -0700

    Refactoring: pulling out common code, making regexes simpler

--- messageParser.py
+++ messageParser.py
@@ -9,23 +9,22 @@ prefix = config.prefix
 # IPv6: :TGB!~user@1A1ED16D:89725881:A8F36EFF:IP PRIVMSG #xkcd-abcdfghijklmnopqrstuvwxyz :ok.
 # Join: :joshztests!43bc0182@hide-6C77D8AA.mibbit.com JOIN :##joshzTestsHisBot 
 
-# Returns the nick string from an IRC message,
-#     validating against the valid nick characters and length.
-# nick: letters, numbers, and the characters -_[]{}\|`^.
-#     The nickname cannot begin with a digit or a hyphen.
-nick = re.compile(r":([^\d\-][A-Za-z0-9\-_[\]{}\\|`^]{0,29})")
+# Returns the nick string from an IRC message
+#   (nicks never contain ! and always have ! following them
+nick = re.compile(r":([^!]+)")
 
 # Get the user-sent message from an IRC PRIVMSG
-message = re.compile(r"[^:]+\ :(.*$)")
+message = re.compile(r":[^:]+\ :(.*$)")
 
 # Get the type of message from an IRC message
 #   (e.g. PRIVMSG, KICK, etc)
 messageType = re.compile(" ([A-Z]*)")
 
 # Get the channel a message was sent to from an IRC message
-channel = re.compile("\S+ \S+ (\S+)")
+channel = re.compile("\S+ \S+ (\S+)")  # \S is any non-whitespace char
 
-# Determines whether the sent message is a PING;
+# Determines whether the sent message is a PING
+# PING :irc.foonet.bar
 ping = re.compile("^PING :(.*?)\r")
 
 # In a JOIN message, determines the chan being joined to.
@@ -42,127 +41,89 @@ partNoArgs = re.compile(prefix + "part(\r| [^&#+!].*)")
 partQuitMessage = re.compile(prefix + "(?:quit|part)(?: [#&+!]\w+)* (.*?)\r")
 
 # Given a user message, determine whether it's a command to the bot to quit
-quitPattern = re.compile("^" + prefix + "quit")
+quit = re.compile("^" + prefix + "quit")
 
 # Get the channel from which to part, given a part command from a user
 channelToPart = re.compile("part ([&#+!][^, :]+)")
 
+def getGroup(match, group=1):
+    '''Given a match for a regex, returns the
+       specified group or "" if there is no match.
+    '''
+    return "" if match is None else match.group(group)
 
 def getMessage(ircMessage):
     '''Given an IRC message, returns the message being sent by the user.
-    
     '''
-    messageMatch = re.search(message, ircMessage)
-    try:
-        return messageMatch.group(1)
-    except:
-        return ""
+    return getGroup(re.search(message, ircMessage))
 
 def hasPrefix(ircMessage):
-    '''Returns true iff the user-sent message has our command prefix.
-    
+    '''Returns true iff the user-sent message has the command prefix.
     '''
     message = getMessage(ircMessage)
     return message.startswith(prefix)
 
 def getNick(ircMessage):
     '''Given an IRC message, returns the nick string (only if it's valid).
-    
     '''
-    nickMatch = re.search(nick, ircMessage)
-    try:
-        return nickMatch.group(1)
-    except:
-        return ""
-        
+    return getGroup(re.search(nick, ircMessage))
+
 def getMessageType(ircMessage):
-    '''Given a properly-formatted IRC message, return type of message
-        (e.g. PRIVMSG, INVITE, etc)
-        
+    '''Given an IRC message, return type of message (PRIVMSG, INVITE, etc)
     '''
-    messageTypeMatch = re.search(messageType, ircMessage)
-    try:
-        return messageTypeMatch.group(1)
-    except:
-        return ""
-        
+    return getGroup(re.search(messageType, ircMessage))
+
 def getChannel(ircMessage):
     '''Given an IRC message, return the channel in which it was sent.
-    
     '''
-    channelMatch = re.search(channel, ircMessage)
-    try:
-        return channelMatch.group(1)
-    except:
-        return ""
+    return getGroup(re.search(channel, ircMessage))
 
 def pingHandler(ircMessage):
-    '''Given an IRC message,
-    return the server to PONG if it's a PING, else the empty string.
-    
+    '''Given an IRC message, return the server to PONG if it's a PING,
+       else the empty string.
     '''
-    pingMatch = re.search(ping, ircMessage)
-    try:
-        return pingMatch.group(1)
-    except:
-        return ""
+    return getGroup(re.search(ping, ircMessage))
 
 def chanFromJoin(ircMessage):
     '''Given an IRC message, return the chan that was joined if it's a JOIN,
-    else the empty string.
-
+       else the empty string.
     '''
-    joinMatch = re.search(join, ircMessage)
-    try:
-        return joinMatch.group(1)
-    except:
-        return ""
+    return getGroup(re.search(join, ircMessage))
 
 def isPart(ircMessage):
     '''Returns true if and only if the given message is a part command
-    
     '''
-    message = getMessage(ircMessage)
-    res = re.search(part, message)
-    if res == None:
-        return False
-    else:
-        return True
-    
+    partMatch = re.search(part, getMessage(ircMessage))
+
+    return partMatch is not None
+
 def partDefault(ircMessage):
     '''Returns true iff the message is a part command without
         channel arguments
-        
     '''
-    message = getMessage(ircMessage)
-    res = re.search(partNoArgs, message)
-    if res == None:
-        return False
-    else:
-        return True
+    partNoArgsMatch = re.search(partNoArgs, getMessage(ircMessage))
+
+    return partNoArgsMatch is not None
 
 def partOrQuitMessage(ircMessage):
     '''Given a message which is a part or quit command,
         find the message to be sent with the quit
-        (e.g. QUIT Message text -> Message text)'''
-    partQuitMessageMatch = re.search(partQuitMessage, ircMessage)
-    try:
-        return partQuitMessageMatch.group(1)
-    except:
-        return ""
+        (e.g. QUIT Message text -> Message text)
+     '''
+    return getGroup(re.search(partQuitMessage, ircMessage))
 
 def isQuit(ircMessage):
-    '''Returns true if and only if the message is a quit command from user'''
-    res = re.search(quitPattern, getMessage(ircMessage))
-    if res == None:
-        return False
-    else:
-        return True
-    
+    '''Returns true if and only if the message is a quit command from user
+    '''
+    quitMatch = re.search(quit, getMessage(ircMessage))
+    return quitMatch is not None
+
 def getPartChannel(ircMessage):
-    '''Given a part command, find the channel from which we want to part.'''
-    channelToPartMatch = re.search(channelToPart, ircMessage)
-    try:
-        return channelToPartMatch.group(1)
-    except:
-        return ""
+    '''Given a part command, find the channel from which we want to part.
+    '''
+    return getGroup(re.search(channelToPart, ircMessage))
+
+def isPM(ircMessage):
+    '''Return true iff the given command is a private message to just us.
+    '''
+    return getChannel(ircMessage) == config.nick
